0036c0e39bb0a9a34f073e01ac0a5dbb2e32b7ae
foryourselfand <foryourselfand@gmail.com>
2019-02-02 19:06:59 +0300

    some assets and code

---
+++ Space Emoji/Assets/Scripts/MultiplySprites.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplySprites : MonoBehaviour
+{
+    public List<Sprite> Sprites;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetRandomSprite()
+    {
+        spriteRenderer.sprite = Sprites[Random.Range(0, Sprites.Count)];
+    }
+}
