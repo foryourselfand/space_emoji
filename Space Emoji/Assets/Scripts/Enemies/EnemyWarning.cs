2c6123ed37f010ffe0d5434ba5b13c23cf532399
foryourselfand <foryourselfand@gmail.com>
2019-02-17 13:18:02 +0300

    beauty arrow

--- Space Emoji/Assets/Scripts/Enemies/EnemyWarning.cs
+++ Space Emoji/Assets/Scripts/Enemies/EnemyWarning.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using UnityEngine;
 
-public class EnemyWarning : MonoBehaviour
+public class EnemyWarning : ScaleChanger
 {
     public Vector2 offSet;
 
     private void Start()
     {
+        SetCurrent(0);
+
         var localPosition = transform.localPosition;
+        localPosition = new Vector3(localPosition.x + offSet.x, localPosition.y + offSet.y);
+        transform.localPosition = localPosition;
 
         StartCoroutine(Blick());
-        localPosition = new Vector3(localPosition.x + offSet.x, localPosition.y + offSet.y);
-
-        transform.localPosition = localPosition;
     }
 
     private IEnumerator Blick()
     {
-        yield return new WaitForSeconds(1);
+        SetTarget(1);
+        yield return new WaitUntil(IsFinished);
+
+        yield return new WaitForSeconds(0.8F);
+
+        SetTarget(0);
+        yield return new WaitUntil(IsFinished);
+
         Destroy(gameObject);
     }
 }
