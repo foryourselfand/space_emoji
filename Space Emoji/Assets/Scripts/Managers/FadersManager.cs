0c2ed5878985a72d8d1dce266a7628b309f289ce
foryourselfand <foryourselfand@gmail.com>
2019-02-21 12:41:30 +0300

    reformating ui for android

--- Space Emoji/Assets/Scripts/Managers/FadersManager.cs
+++ Space Emoji/Assets/Scripts/Managers/FadersManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class FadersManager : MonoBehaviour
 {
-    public SpriteOpacityChanger sky;
+    public CanvasOpacityChanger sky;
 
     public CanvasOpacityChanger canvasOpacityChanger;
 
